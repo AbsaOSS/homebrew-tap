0879f06dc79bcccbe2195e31e01f63aefd9b31b1
Sasa Zejnilovic <zejnils@gmail.com>
2022-09-11 21:34:28 +0200

    #1 Add formula for spark 2.4.6

---
+++ Formula/apache-spark@2.4.6.rb
@@ -0,0 +1,44 @@
+# Copyright 2022 ABSA Group Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+class ApacheSparkAT246 < Formula
+  desc "Engine for large-scale data processing"
+  homepage "https://spark.apache.org/"
+  url "https://www.apache.org/dyn/closer.lua?path=spark/spark-2.4.6/spark-2.4.6-bin-hadoop2.7.tgz"
+  mirror "https://archive.apache.org/dist/spark/spark-2.4.6/spark-2.4.6-bin-hadoop2.7.tgz"
+  version "2.4.6"
+  sha256 "7174fee30057fbf226698ebfcfa7c2944a26197e149053aa29b26b250d1e1eba"
+  license "Apache-2.0"
+  head "https://github.com/apache/spark.git"
+
+  # TODO add bottle
+
+  keg_only :versioned_formula
+
+  depends_on "openjdk@8"
+
+  def install
+    # Rename beeline to distinguish it from hive's beeline
+    mv "bin/beeline", "bin/spark-beeline"
+    rm_f Dir["bin/*.cmd"]
+    libexec.install Dir["*"]
+    bin.install Dir[libexec/"bin/*"]
+    bin.env_script_all_files(libexec/"bin", Language::Java.java_home_env("1.8"))
+  end
+
+  test do
+    assert_match "Long = 1000",
+      pipe_output(bin/"spark-shell --conf spark.driver.bindAddress=127.0.0.1",
+                  "sc.parallelize(1 to 1000).count()")
+  end
+end
